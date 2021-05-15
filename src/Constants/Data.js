9f5c5b59d30fa9e93237a11119c8ac8bceb535b5
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2021-05-15 19:08:53 +0600

    key update

--- src/Constants/Data.js
+++ src/Constants/Data.js
@@ -1,6 +1,6 @@
 const data = {
   github_username: "TanvirMahin24",
-  tkn: "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",
+  tkn: "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",
   base: "https://api.github.com/graphql",
   sec: "f9b4857b852df4723a7a44db63cef7a772600f64",
   id: "44f94358fcee34f3fe16",
