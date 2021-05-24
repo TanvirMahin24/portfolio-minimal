d966c3bb04d32e60df5356a2f651fcad26bc37c1
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2021-05-24 17:15:25 +0600

    file added

--- src/Constants/Data.js
+++ src/Constants/Data.js
@@ -1,6 +1,7 @@
+import { GITHUB_AUTH_TOKEN } from "./secreat.s";
 const data = {
   github_username: "TanvirMahin24",
-  tkn: "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",
+  tkn: GITHUB_AUTH_TOKEN,
   base: "https://api.github.com/graphql",
   sec: "f9b4857b852df4723a7a44db63cef7a772600f64",
   id: "44f94358fcee34f3fe16",
