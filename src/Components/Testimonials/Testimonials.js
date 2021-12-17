62f08564d8dd8cbb4ea9c1f2eb3881675cbdec7e
Tanvir Mahin <tanvirmahin24@gmail.com>
2021-12-17 19:32:13 +0600

    what people say done

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -4,51 +4,58 @@ import data from "../../stub/Testimonials/data";
 import TestimonialItem from "./TestimonialItem/TestimonialItem";
 import { Row, Col } from "react-bootstrap";
 
-const Testimonials = () => {
-  const [selectedData, setSelectedData] = useState(0);
-  const [inProp, setInProp] = useState(true);
-  useEffect(() => {
-    const stateChangeFunc = () => {
-      setInProp(false);
-      setTimeout(() => {
-        if (selectedData + 1 >= data.length) {
-          setSelectedData(0);
-        } else {
-          setSelectedData(selectedData + 1);
-        }
-        // console.log(selectedData);
-      }, 5000);
-      setInProp(true);
-    };
-    stateChangeFunc();
-  }, [selectedData]);
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
+import "swiper/swiper.min.css";
+import "swiper/modules/pagination/pagination.min.css";
+import "swiper/modules/effect-cube/effect-cube.min.css";
 
+// import Swiper core and required modules
+import SwiperCore, { EffectCoverflow, Pagination, Autoplay } from "swiper";
+
+// install Swiper modules
+SwiperCore.use([EffectCoverflow, Pagination, Autoplay]);
+
+const Testimonials = () => {
   return (
-    <Row>
-      <Col md={12}>
-        <h1 className={styles.title}>What People Say</h1>
-      </Col>
-      <Col md={6}>
-        {inProp && <TestimonialItem inProp={inProp} {...data[selectedData]} />}
-      </Col>
-      <Col md={6} className="d-md-block d-none">
-        <TestimonialItem
-          inProp={inProp}
-          {...data[selectedData == data.length - 1 ? 0 : selectedData + 1]}
-        />
-      </Col>
-      <Col md={12}>
-        <div className="d-flex justify-content-center align-items-center pt-4">
-          {data.map((item) => (
-            <div
-              className={
-                item.id - 1 === selectedData ? styles.activeDot : styles.dot
-              }
-            ></div>
-          ))}
-        </div>
-      </Col>
-    </Row>
+    <>
+      <h1 className={styles.title}>What People Say</h1>
+
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        loop={true}
+        coverflowEffect={{
+          rotate: 60,
+          stretch: 0,
+          depth: 100,
+          modifier: 0.8,
+          slideShadows: true,
+        }}
+        autoplay={{ delay: 2500, disableOnInteraction: true }}
+        pagination={false}
+        className="mySwiper"
+        breakpoints={{
+          "@0.00": {
+            slidesPerView: 1,
+          },
+          "@0.75": {
+            slidesPerView: 2,
+          },
+          "@1.00": {
+            slidesPerView: 3,
+          },
+          "@1.50": {
+            slidesPerView: 3,
+          },
+        }}
+      >
+        {data.map((item) => (
+          <SwiperSlide>
+            <TestimonialItem {...item} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 };
 
